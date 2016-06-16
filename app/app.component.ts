c3819ff44c8cc1b496b2fd95978514acf1052783
anoopgoudar <anoop.a.goudar@gmail.com>
2016-06-16 11:42:53 -0700

    Click functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,43 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
+export class Hero {
+  id: number;
+  name: string;
+}
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+        <span>{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+     <h2>{{selectedHero.name}} details!</h2>
+     <div><label>id: </label>{{selectedHero.id}}</div>
+     <div>
+       <label>name: </label>
+       <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+     </div>
+   </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+title = 'Tour of Heroes';
+heroes = HEROES;
+selectedHero: Hero;
+onSelect(hero: Hero) { this.selectedHero = hero; }
+}
