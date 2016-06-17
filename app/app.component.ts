324bbd8f2a6349d4401a166dc23d7cc1bfee940a
anoopgoudar <anoop.a.goudar@gmail.com>
2016-06-16 21:06:39 -0700

    check

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ export class Hero {
   name: string;
 }
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice :)' },
+  { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
