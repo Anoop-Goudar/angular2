c3b6c838425a9cf3371078e42d6dc5890dd73cfa
anoopgoudar <anoop.a.goudar@gmail.com>
2016-06-24 05:41:42 -0700

    Multiple routing with hero detials

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html',
+  directives: [HeroDetailComponent],
   providers: [HeroService]
 
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  selectedHero: Hero;
+
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.heroes = this.heroService.getHeroes().slice(1,5);
     //this.heroService.getHeroes().then(heroesResponse => this.heroes = heroesResponse.slice(1,5));
   }
+
+  gotoDetail(hero: Hero) { this.selectedHero = hero; }
 
 }
